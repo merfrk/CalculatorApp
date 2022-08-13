9108286649a79d50a90c344ab69e3d30d419a257
Ömer Faruk ARTAN <o.farukartan@hotmail.com>
2022-08-13 23:57:26 +0300

    create UI

--- BasicCalculator/ViewController.swift
+++ BasicCalculator/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var clearButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func changeAC(_ sender: Any) {
+        let font = UIFont(name: "Helvetica", size: 30)
+        clearButton.setAttributedTitle(NSAttributedString(string: "C", attributes: [NSAttributedString.Key.font: font!]), for: .normal)
+        
+    }
+    
+    
+    @IBAction func clear(_ sender: UIButton) {
+        let font = UIFont(name: "Helvetica", size: 30)
+        clearButton.setAttributedTitle(NSAttributedString(string: "AC", attributes: [NSAttributedString.Key.font: font!]), for: .normal)
+    }
+    
+    
 }
 
