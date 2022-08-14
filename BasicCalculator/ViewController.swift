874acd01a12fcd7e0a63eb99dd50da816e74f989
Ömer Faruk ARTAN <o.farukartan@hotmail.com>
2022-08-14 18:23:14 +0300

    create addition

--- BasicCalculator/ViewController.swift
+++ BasicCalculator/ViewController.swift
@@ -9,25 +9,91 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    @IBOutlet var resultLabel: UILabel!
     @IBOutlet var clearButton: UIButton!
+    
+    var firstNumber: Double?
+    var secondNumber: Double?
+    var firstPart: String?
+    var secondPart: String?
+    var result: Double?
+    var operationType: operation?
+    
+    enum operation {
+        case addition
+        case subtraction
+        case multiplication
+        case division
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
     
-    @IBAction func changeAC(_ sender: Any) {
+    @IBAction func numberPadClicked(_ sender: UIButton) {
         let font = UIFont(name: "Helvetica", size: 30)
-        clearButton.setAttributedTitle(NSAttributedString(string: "C", attributes: [NSAttributedString.Key.font: font!]), for: .normal)
         
+        if resultLabel.text! == "0" && sender.titleLabel?.text! != "."{
+            resultLabel.text! = ""
+        }
+        
+        resultLabel.text! += (sender.titleLabel?.text!)!
+        if clearButton.titleLabel?.text! == "AC"{
+            clearButton.setAttributedTitle(NSAttributedString(string: "C", attributes: [NSAttributedString.Key.font: font!]), for: .normal)
+        }
     }
     
-    
     @IBAction func clear(_ sender: UIButton) {
+        resultLabel.text! = "0"
+        
         let font = UIFont(name: "Helvetica", size: 30)
-        clearButton.setAttributedTitle(NSAttributedString(string: "AC", attributes: [NSAttributedString.Key.font: font!]), for: .normal)
+        if clearButton.titleLabel?.text! == "C"{
+            clearButton.setAttributedTitle(NSAttributedString(string: "AC", attributes: [NSAttributedString.Key.font: font!]), for: .normal)
+        }
+    }
+    
+    @IBAction func reverseSign(_ sender: Any) {
+        let i = resultLabel.text!.index(resultLabel.text!.startIndex, offsetBy: 0)
+        
+        if resultLabel.text!.hasPrefix("-"){
+            resultLabel.text!.remove(at: i)
+        }else{
+            resultLabel.text!.insert("-", at: i)
+        }
+    }
+    
+    @IBAction func percentClicked(_ sender: Any) {
+        var newResult = Double(resultLabel.text!)!
+        newResult /= 100
+        resultLabel.text! = String(newResult)
+    }
+    
+    @IBAction func addClicked(_ sender: Any) {
+        firstPart = resultLabel.text!
+        resultLabel.text! = ""
+        firstNumber = Double(firstPart!)!
+        operationType = .addition
+    }
+    
+    @IBAction func calculate(_ sender: Any) {
+        secondPart = String(resultLabel.text!)
+        secondNumber = Double(secondPart!)!
+        
+        switch operationType{
+        case .addition:
+            result = firstNumber! + secondNumber!
+        case .none:
+            print("")
+        case .subtraction:
+            print("")
+        case .multiplication:
+            print("")
+        case .division:
+            print("")
+        }
+        
+        resultLabel.text! = String(result!)
     }
     
     
